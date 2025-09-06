86d073a478cca170528514b2570e7ffba0257144
vercingetorx <40043405+vercingetorx@users.noreply.github.com>
2025-09-06 13:57:30 -0700

    Add files via upload

---
+++ deobfuscated.js
@@ -0,0 +1,26 @@
+"use strict";
+
+(self.webpackChunkof_vue = self.webpackChunkof_vue || []).push([
+  [2313], {
+    802313: function(W, n, o) {
+      var t = o(944114),
+        c = o.n(t),
+        r = o(89668),
+        u = o.n(r),
+        d = o(858156),
+        e = o.n(d),
+        k = o(441153);
+      n.A = W => {
+        const o = W.url || "",
+          t = window.navigator.userAgent || null,
+          c = {};
+        c.time = +new Date();
+        const r = k.A.getters["auth/authUserId"] || null,
+          d = u()(["IjPESe2BMUL892ZTKVXh6e98Jc3KQf0c", c.time, o, r || 0].join("\n"));
+        return c.sign = ["36031", d, function(W) {
+          return Math.abs(W[37618 % W.length].charCodeAt(0) + 74 + (W[37281 % W.length].charCodeAt(0) - 76) + (W[37779 % W.length].charCodeAt(0) - 104) + (W[36874 % W.length].charCodeAt(0) - 111) + (W[36773 % W.length].charCodeAt(0) - 62) + (W[37505 % W.length].charCodeAt(0) + 51) + (W[36487 % W.length].charCodeAt(0) - 58) + (W[36586 % W.length].charCodeAt(0) - 109) + (W[37206 % W.length].charCodeAt(0) + 108) + (W[38305 % W.length].charCodeAt(0) + 81) + (W[38484 % W.length].charCodeAt(0) - 93) + (W[37032 % W.length].charCodeAt(0) + 112) + (W[36178 % W.length].charCodeAt(0) + 66) + (W[38156 % W.length].charCodeAt(0) - 95) + (W[37694 % W.length].charCodeAt(0) + 106) + (W[36660 % W.length].charCodeAt(0) + 94) + (W[36361 % W.length].charCodeAt(0) - 90) + (W[37365 % W.length].charCodeAt(0) + 105) + (W[36716 % W.length].charCodeAt(0) + 68) + (W[38073 % W.length].charCodeAt(0) - 126) + (W[36280 % W.length].charCodeAt(0) + 126) + (W[38411 % W.length].charCodeAt(0) + 117) + (W[38225 % W.length].charCodeAt(0) - 86) + (W[37120 % W.length].charCodeAt(0) + 88) + (W[37922 % W.length].charCodeAt(0) + 68) + (W[36130 % W.length].charCodeAt(0) - 101) + (W[36436 % W.length].charCodeAt(0) - 95) + (W[37455 % W.length].charCodeAt(0) + 105) + (W[37856 % W.length].charCodeAt(0) - 77) + (W[36928 % W.length].charCodeAt(0) + 54) + (W[37562 % W.length].charCodeAt(0) - 83) + (W[37972 % W.length].charCodeAt(0) + 70)).toString(16);
+        }(d), "67922a28"].join(":"), c;
+      };
+    }
+  }
+]);
